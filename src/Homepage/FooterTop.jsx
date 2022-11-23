2dfe10253bdbddf147d1ac6fc2ae94b2b81a0988
JeffTam33 <53273407+JeffTam33@users.noreply.github.com>
2022-11-22 19:36:17 -0500

    Update FooterTop.jsx

--- src/Homepage/FooterTop.jsx
+++ src/Homepage/FooterTop.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import '../CssFile/LightMode/FooterTop.css'
 import { UilTruck, UilUsersAlt, UilCube } from '@iconscout/react-unicons'
-
+//New
 function Footer() {
 
   return (
